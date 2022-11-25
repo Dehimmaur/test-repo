03a0d4ca2835d14bacc0eee0668351b2a49751c2
Dehimmaur <dehimmaur@gmail.com>
2022-11-25 21:27:28 +0300

    333

---
+++ lvl3.py
@@ -0,0 +1,13 @@
+
+
+n1 = input('Введите первое число: ')
+n2 = input('Введите второе число: ')
+
+n1 = float(n1)
+n2 = float(n2)
+
+
+if n1 > n2:
+	print(n1)
+else:
+	print(n2)
--- new_file.py
+++ new_file.py
@@ -1,1 +1,14 @@
-print('Hello world')
+v = input('Введите скорость: ')
+h = input('Введите время: ')
+
+v = int(v)
+h = int(h)
+
+L = h*v 
+
+if L >=109:
+	L = L - 109
+
+print('Вы остановитесь на отметке: ' + str(L))
+
+
